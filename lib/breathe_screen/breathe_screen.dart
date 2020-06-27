64fd7b8ebf911254e1fe770d596195cd233bd56c
Nolan Sherman <nolanrsherman@gmail.com>
2020-06-27 07:21:49 -0500

    added simple model for breathing session

--- lib/breathe_screen/breathe_screen.dart
+++ lib/breathe_screen/breathe_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+//model for a breathing session
+class BreatheSession {
+  int inBreaths; //how many in breaths
+  int outBreaths; //how many out breaths
+  double sessionLengthSeconds; //total length of session
+}
+
 class BreatheScreen extends StatefulWidget {
   BreatheScreen() : super();
 
